6a1243c61b01554aee4d9375c0a3f9c19579b007
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-26 12:40:55 -0700

    refactored code using the split variable refactoring

--- refactor_rec.rb
+++ refactor_rec.rb
@@ -1,8 +1,8 @@
 class Rectangle
     def print_area_and_parameter()
-        temp = width * length
+        area = width * length
         puts "The area of the rectangle is #{temp}}"
-        temp = (@width * 2) + (@length * 2)
+        parameter = (@width * 2) + (@length * 2)
         puts "The perimeter of the rectangle is #{temp}"
     end
 end
